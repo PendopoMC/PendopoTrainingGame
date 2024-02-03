d8377f68aacecc64eccdd95d7d24f86be4e83bd7
MT Akbar <muhammadt.akbar@gmail.com>
2024-02-03 11:39:43 +0700

    Initial commit 2022.3.17f1
    MVC
    Ink
    DotTween
    Cinemachine
    URP
    NaughtyAttributes

---
+++ TraningGame/Assets/@PendopoMVC/Boot/GameMain.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Agate.MVC.Core;
+using Agate.MVC.Base;
+
+namespace Pendopo.TraningGame.Boot
+{
+    public class GameMain : BaseMain<GameMain>, IMain
+    {
+        protected override IConnector[] GetConnectors()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IController[] GetDependencies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IEnumerator StartInit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
