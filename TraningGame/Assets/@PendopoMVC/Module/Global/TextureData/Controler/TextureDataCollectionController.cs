3df8f9e701b01ee9eaf47b3829b8ffffe916da12
MT Akbar <muhammadt.akbar@gmail.com>
2024-04-02 16:55:00 +0700

    Background

--- TraningGame/Assets/@PendopoMVC/Module/Global/TextureData/Controler/TextureDataCollectionController.cs
+++ TraningGame/Assets/@PendopoMVC/Module/Global/TextureData/Controler/TextureDataCollectionController.cs
@@ -13,14 +13,12 @@ namespace Pendopo.TraningGame.Global.TextureDataCollection
 
     public class TextureDataCollectionController : DataController<TextureDataCollectionController,TextureDataCollectionModel>
     {
-        RequestTextureCallback textureCallback;
         Texture2D clearTexture;
         public override IEnumerator Initialize()
         {
             yield return base.Initialize();
             Debug.Log("Load all Texture");
             GetAllTexture();
-            textureCallback = new RequestTextureCallback();
             clearTexture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
             clearTexture.SetPixel(0, 0, new Color(0, 0, 0, 0)); // Transparent pixel
             clearTexture.Apply();
@@ -45,38 +43,5 @@ namespace Pendopo.TraningGame.Global.TextureDataCollection
             if (!_model.packageTexture.ContainsKey(_data)) return clearTexture;
             return _model.packageTexture[_data];
         }
-    }
-}
-
-
-namespace Pendopo.TraningGame.Message
-{
-    public struct RequestTexture
-    {
-        public ObjectData data;
-    }
-
-    public struct RequestTextureCallback
-    {
-        public Texture basePackage;
-        public Texture tx_barCode;
-        public Texture tx_qrCode;
-        public Texture tx_komposisiEng;
-        public Texture tx_komposisiInd;
-        public Texture tx_logoHalal;
-        public Texture tx_logoPilihan;
-        public Texture tx_namaRasaDepan;
-        public Texture tx_namaRasaBelakang;
-        public Texture tx_logoRasaDepan;
-        public Texture tx_logoRasaBelakaang;
-        public Texture tx_berat;
-        public Texture tx_kodeProduksi;
-        public Texture tx_logoRecycle;
-        public Texture tx_exp;
-        public Texture tx_seal;
-        public Texture tx_sedotan;
-        public Texture tx_contact;
-        public Texture tx_nutrisi;
-        public Texture tx_peringatan;
     }
 }
